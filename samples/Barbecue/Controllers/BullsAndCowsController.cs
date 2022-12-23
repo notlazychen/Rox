b9a0f576475c793ff6202e9c68cab910eadebc40
roy <crazor@foxmail.com>
2022-12-23 17:37:07 +0800

    猜数字的前端开始开发

---
+++ samples/Barbecue/Controllers/BullsAndCowsController.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Rox.Extensions.BullsAndCows;
+using System.Collections.Concurrent;
+
+namespace Barbecue.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class BullsAndCowsController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        public BullsAndCowsController(ILogger<WeatherForecastController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            this._memoryCache = memoryCache;
+        }
+
+        //[HttpGet()]
+        //public GameInfo GetGameInfo()
+        //{
+        //    return null;
+        //}
+
+        [HttpPost]
+        public ActionResult Start()
+        {
+            var game = new Game();
+            _memoryCache.Set("Game_" + UserId, game, DateTimeOffset.Now.AddDays(1));
+            var robot = new Robot();
+            robot.Start(game.Options);
+            _memoryCache.Set("Robot_" + UserId, robot, DateTimeOffset.Now.AddDays(1));
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<Result> Guess([FromBody] GuessRequest req)
+        {
+            var game = _memoryCache.Get<Game>("Game_" + UserId);
+            var result = game.Guess(req.GuessNumber);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public ActionResult<Result> AskRobot([FromBody] ReplyRobotRequest req)
+        {
+            var robot = _memoryCache.Get<Robot>("Robot_" + UserId);
+            robot.Return(Result.Parse(req.Result));
+            var answer = robot.GetNextAnswer();
+            return Ok(answer);
+        }
+
+        private string UserId { get { return this.HttpContext.Connection.RemoteIpAddress + ":" + this.HttpContext.Connection.RemotePort; } }
+    }
+
+    public class ReplyRobotRequest
+    {
+        public string Result { get; set; }
+    }
+
+
+    public class GuessRequest
+    {
+        public string GuessNumber { get; set; }
+    }
+
+    public class GameInfo
+    {
+        public int CurrentRound { get; set; }
+        public List<RoundInfo> History { get; set; } = new List<RoundInfo>();
+
+    }
+
+    public class RoundInfo
+    {
+        public int Round { get; set; }
+        public List<Chance> Chances { get; set; } = new List<Chance>();
+    }
+
+    public class Chance
+    {
+        public string Name { get; set; }
+        public string GuessNum { get; set; }
+        public string Result { get; set; }
+    }
+}
---
+++ samples/Barbecue/ExceptionMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Barbecue
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception ex)
+            {
+                httpContext.Response.StatusCode = 400;
+                await httpContext.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
